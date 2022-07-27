538fcdd9e4ea2fe85e06dff19570c207ad679d77
Kirill <sonik3000.83@gmail.com>
2022-07-27 19:41:16 +0300

    initial commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,93 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class StatsServiceTest {
+
+    @Test
+    public void countingAllsales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+
+        Assertions.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void averageMonthlysales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.average(sales);
+        long expected = 15;
+
+
+        Assertions.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void countBelowaverage() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.countBelowAvg(sales);
+        long expected = 5;
+
+
+        Assertions.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void subsetAboveAverage() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.aboveAverage(sales);
+        long expected = 7;
+
+
+        Assertions.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void minimumSalesVolume() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.minSales(sales);
+        long expected = 9;
+
+
+        Assertions.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void maximumSalesVolume() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.maxSales(sales);
+        long expected = 8;
+
+
+        Assertions.assertEquals(actual, expected);
+
+    }
+}
+
